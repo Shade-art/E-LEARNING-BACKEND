cd4e7592bbb02459a5eb4d3179a55c1fa3d28975
Shade art <hamzabitmesra786@gmail.com>
2026-07-05 02:05:32 +0530

    Created a Entity of Java Object

--- src/main/java/org/example/jpa/models/Author.java
+++ src/main/java/org/example/jpa/models/Author.java
@@ -1,16 +1,26 @@
 package org.example.jpa.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
 
 
-@Data //this contains getters and setters + toString + equals and hashcode comparison its ALL IN ONE
+@Data //this contains getters and setters + toString + equals and hashcode comparison it's ALL IN ONE
 @NoArgsConstructor
 @Entity //Hibernate detects this
 public class Author {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "author_sequence" //give name to our sequence but hibernate won't find it
+    )
+    @SequenceGenerator(
+            name = "author_sequence", //same as generator
+            sequenceName = "Suiiiiiiii",//any name
+            allocationSize = 1
+    )
+
     private Integer id; //why Integer as its default value is null and int default value is 0
 
     private String firstName;
